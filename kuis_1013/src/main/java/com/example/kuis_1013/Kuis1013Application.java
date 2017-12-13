46393511f3f75dca548d72bc89fc5b497423ce92
Hulahula6661 <>
2017-12-13 16:25:35 +0700

    Kuis2

---
+++ kuis_1013/src/main/java/com/example/kuis_1013/Kuis1013Application.java
@@ -0,0 +1,12 @@
+package com.example.kuis_1013;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kuis1013Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kuis1013Application.class, args);
+	}
+}
